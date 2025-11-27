3ffde7efafc54866f16b274951a819607e310364
Jason bernal <yavzlh@gmail.com>
2025-11-27 20:01:23 +0100

    comit ts pluginsss

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,8 +1,9 @@
+// path: ./config/plugins.ts
 export default ({ env }) => ({
   'users-permissions': {
     enabled: true,
     config: {
-      jwtSecret: env('JWT_SECRET'),
+      jwtSecret: env('JWT_SECRET', 'DEV_FALLBACK_SECRET_123'),
     },
   },
 });
