4ee710c7b38626ea61733b0fde22bdafc3d384fc
Jason bernal <yavzlh@gmail.com>
2025-11-28 19:16:50 +0100

    1.3

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,47 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:'],
+          'media-src': ["'self'", 'data:', 'blob:'],
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'https://nails-zeta.vercel.app',
+        'http://localhost:3000',
+        'http://localhost:3001'
+      ],
+      headers: [
+        'Content-Type', 
+        'Authorization', 
+        'Origin', 
+        'Accept', 
+        'X-Requested-With'
+      ],
+      methods: [
+        'GET', 
+        'POST', 
+        'PUT', 
+        'PATCH', 
+        'DELETE', 
+        'HEAD', 
+        'OPTIONS'
+      ],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
